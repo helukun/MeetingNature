a3013786b97f42449940197904572533c05f2810
ANGELOV-JAMES <85118303+ANGELOV-JAMES@users.noreply.github.com>
2022-12-11 17:41:01 +0800

    Sponsor_v1.1

---
+++ src/main/java/com/example/sponsor_microservice/controller/SponsorController.java
@@ -0,0 +1,43 @@
+package com.example.sponsor_microservice.controller;
+
+import com.example.sponsor_microservice.service.SponsorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SponsorController {
+    @Autowired
+    private SponsorService sponsorService;
+
+    //good
+    @GetMapping("/v1.0/sponsor-microservice/projects/RPInfo")
+    public Object disRPInfo(String size){
+        return sponsorService.disRPInfo(size);
+    }
+
+    //good
+    @GetMapping("/v1.0/sponsor-microservice/projects/page")
+    public Object findProjectByPage(int index,int pageSize){
+        return sponsorService.findProjectByPage(index,pageSize);
+    }
+
+    //good
+    @GetMapping("/v1.0/sponsor-microservice/projects/projectAndNotice")
+    public Object findProjectAndNotice(String id){
+        return sponsorService.findProjectAndNotice(id);
+    }
+
+    //good
+    @GetMapping("/v1.0/sponsor-microservice/feedback/all")
+    public List findFeedBackInfoByPage(String index,String pageSize,String sponsorId){
+        return sponsorService.findFeedBackInfoByPage(index,pageSize,sponsorId);
+    }
+    @PostMapping("/v1.0/sponsor-microservice/follow/add")
+    public void addFollow(String followerId,String subjectId){
+        sponsorService.addFollow(followerId,subjectId);
+    }
+}
---
+++ src/main/java/com/example/sponsor_microservice/service/SponsorService.java
@@ -0,0 +1,105 @@
+package com.example.sponsor_microservice.service;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.github.kevinsawicki.http.HttpRequest;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SponsorService {
+    private String ProjectMicroserviceIp="http://localhost:9006";
+    private String FollowMicroserviceIp="http://localhost:9008";
+    private String ProcessManagementMicroserviceIp="http://localhost:9005";
+    public Object disRPInfo(String size) {
+        String resp =HttpRequest.get(ProjectMicroserviceIp+"/v1.1/project-microservice/projects/random?size="+size).body();
+        return JSON.parse(resp);
+    }
+
+    public Object findProjectByPage(int index,int pageSize){
+        String resp=HttpRequest.get(ProjectMicroserviceIp
+                + "/v1.1/project-microservice/projects/page?index="
+                +index+"&pageSize="
+                +pageSize).body();
+        return JSON.parse(resp);
+    }
+
+    public Object findProjectAndNotice(String id){
+        String resp1 =HttpRequest.get(ProjectMicroserviceIp+"/v1.1/project-microservice/projects/Id?id="+id).body();
+        String resp2 =HttpRequest.get(FollowMicroserviceIp+"/v1.1/follow-microservice/notice/subjectId?subjectId="+id).body();
+        System.out.println(resp1.substring(0,resp1.lastIndexOf('}'))+','+"\"notice\":"+resp2+'}');
+        Object obj=JSON.parse( resp1.substring(0,resp1.lastIndexOf('}'))+','+"\"notice\":"+resp2+'}');
+        return obj;
+    }
+
+    //CAONIMA
+    public List findFeedBackInfoByPage(String index,String pageSize,String sponsorId){
+        String resp1 =HttpRequest.get(ProcessManagementMicroserviceIp+"/v1.1/processmanagement-microservice/feedback/sponsorId?sponsorId="+sponsorId).body();
+        List tmp= (List) JSON.parse(resp1);
+        List result_list=new ArrayList<>();
+        int size=tmp.size();
+        for(int i=0;i<size;i++){
+            Object o=tmp.get(i);
+            String objS= String.valueOf(o);
+            int pos=objS.indexOf("\"subjectId\"");
+            String subs=objS.substring(pos+13);
+            int firstIndex=subs.indexOf('\"');
+            //找到的subjectId
+            String resId=subs.substring(0,firstIndex);
+            String proResp =HttpRequest.get(ProjectMicroserviceIp+"/v1.1/project-microservice/projects/Id?id="+resId).body();
+            //int startPosOfProjectName=proResp.indexOf("\"projectName\"");
+            String projectNamesub1=proResp.substring(proResp.indexOf("\"projectName\"")+1);
+            String projectNamesub2=projectNamesub1.substring((projectNamesub1.indexOf('\"')+1));
+            String projectNamesub3=projectNamesub2.substring(projectNamesub2.indexOf('\"')+1);
+            String projectName=projectNamesub3.substring(0,projectNamesub3.indexOf('\"'));
+
+            String organizationsub1=proResp.substring(proResp.indexOf("\"projectName\"")+1);
+            String organizationsub2=organizationsub1.substring((organizationsub1.indexOf('\"')+1));
+            String organizationsub3=organizationsub2.substring(organizationsub2.indexOf('\"')+1);
+            String organization=organizationsub3.substring(0,organizationsub3.indexOf('\"'));
+
+            String result=objS.substring(0,objS.lastIndexOf('}'))+','+"\"projectName\":"+'\"'+projectName+'\"'+','+"\"organization\":"+'\"'+organization+'\"'+'}';
+            result_list.add(JSON.parse( result));
+        }
+        int realSize=result_list.size();
+        int pageNum= realSize/Integer.parseInt(pageSize);
+        int rest=realSize%Integer.parseInt(pageSize);
+        List finalList=new ArrayList<>();
+        if(rest==0){
+            if(Integer.parseInt(index)>pageNum){
+                return finalList;
+            }
+            for(int i=(Integer.parseInt(index)-1)*Integer.parseInt(pageSize);i<(Integer.parseInt(index))*Integer.parseInt(pageSize);i++){
+                finalList.add(result_list.get(i));
+            }
+        }
+        else{
+            if(Integer.parseInt(index)<pageNum+1){
+                for(int i=(Integer.parseInt(index)-1)*Integer.parseInt(pageSize);i<(Integer.parseInt(index))*Integer.parseInt(pageSize);i++){
+                    finalList.add(result_list.get(i));
+                }
+            }
+            else if(Integer.parseInt(index)==pageNum+1){
+                for(int i=(Integer.parseInt(index)-1)*Integer.parseInt(pageSize);i<realSize;i++){
+                    finalList.add(result_list.get(i));
+                }
+            }
+            else{
+                return finalList;
+            }
+        }
+        return finalList;
+    }
+
+    public void addFollow(String followerId,String subjectId){
+        Map data = new HashMap();
+        data.put("followerId", followerId);
+        data.put("subjectId", subjectId);
+        String result =HttpRequest.post(FollowMicroserviceIp+"/v1.1/follow-microservice/follow").form(data).body();
+    }
+}
