1056f3facca6515f1cfb27dd7b5bfc7987badf6f
ANGELOV-JAMES <85118303+ANGELOV-JAMES@users.noreply.github.com>
2022-12-15 15:05:50 +0800

    v1.5

--- src/main/java/com/example/sponsored_microservice/service/SponsoredService.java
+++ src/main/java/com/example/sponsored_microservice/service/SponsoredService.java
@@ -27,9 +27,9 @@ import java.util.Map;
 public class SponsoredService {
     @Autowired
     OSSService ossService;
-    private String ProjectMicroserviceIp="http://localhost:9006";
-    private String FollowMicroserviceIp="http://localhost:9008";
-    private String ProcessManagementMicroserviceIp="http://localhost:9005";
+    private String ProjectMicroserviceIp="http://121.5.128.97:9006";
+    private String FollowMicroserviceIp="http://121.5.128.97:9008";
+    private String ProcessManagementMicroserviceIp="http://121.5.128.97:9005";
 
     public Object findProByOrg(String organization){
         String resp=HttpRequest.get(ProjectMicroserviceIp+"/v1.1/project-microservice/projects/organization?organization="+
