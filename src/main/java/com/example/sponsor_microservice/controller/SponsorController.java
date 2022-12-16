553115015a3d653d2d595defa083f77c22249541
ANGELOV-JAMES <85118303+ANGELOV-JAMES@users.noreply.github.com>
2022-12-16 19:53:16 +0800

    v1.6

--- src/main/java/com/example/sponsor_microservice/controller/SponsorController.java
+++ src/main/java/com/example/sponsor_microservice/controller/SponsorController.java
@@ -2,12 +2,14 @@ package com.example.sponsor_microservice.controller;
 
 import com.example.sponsor_microservice.service.SponsorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class SponsorController {
     @Autowired
