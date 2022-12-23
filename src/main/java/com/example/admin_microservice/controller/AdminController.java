141bb83f3602790361bbf25674ba968835b85d4f
LJCARG <83816521+LJCARG@users.noreply.github.com>
2022-12-24 02:19:01 +0800

    Add files via upload

--- src/main/java/com/example/admin_microservice/controller/AdminController.java
+++ src/main/java/com/example/admin_microservice/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.example.admin_microservice.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.admin_microservice.service.AdminService;
+import com.github.kevinsawicki.http.HttpRequest;
 import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +35,41 @@ public class AdminController {
     @PutMapping("/v2.0/admin-microservice/projects/status/yellow")
     public void ProjectStatusToYellow(String projectId){
         adminService.ProjectStatusToYellow(projectId);
+    }
+    //good
+    @GetMapping("/v2.0/admin-microservice/projects/id")
+    public Object FindProByProId(String id){
+        return adminService.findProByProId(id);
+    }
+    //good
+    @GetMapping("/v2.0/admin-microservice/feedbcks/id")
+    public Object FindFBByProId(String subjectId){
+        return adminService.findFBByProId(subjectId);
+    }
+    //good
+    @GetMapping("/v2.0/admin-microservice/notices/id")
+    public Object FindNoByProId(String subjectId){
+        return adminService.findNotByProId(subjectId);
+    }
+    //good
+    @GetMapping("/v2.0/admin-microservice/conplaints")
+    public Object GetAllComplaint(){
+        return adminService.GetAllComplaint();
+    }
+    //good
+    @PutMapping("/v2.0/admin-microservice/conplaints/status")
+    public void ChangComplaintStatusGreen(String id){
+        adminService.ChangComplaintStatusGreen(id);
+    }
+    //good
+    @DeleteMapping("/v2.0/admin-microservice/conplaints")
+    public void DeleteComplaint(String id){
+        adminService.DeleteComplaint(id);
+    }
+
+    @GetMapping("/v2.0/admin-microservice/subject/yellow")
+    public Object GetAllYellowProject(String index,String pageSize) {
+        return adminService.GetAllYellowProject(index,pageSize);
     }
 
 }
--- src/main/java/com/example/admin_microservice/service/AdminService.java
+++ src/main/java/com/example/admin_microservice/service/AdminService.java
@@ -18,7 +18,10 @@ import java.util.Map;
 public class AdminService {
 
     private String AuditMicroserviceIp = "http://121.5.128.97:9011";
-
+    private  String ProjectMicroserviceIp="http://121.5.128.97:9006";
+    private  String ComplaintMicroserviceIp="http://121.5.128.97:9013";
+    private String FollowMicroserviceIp="http://121.5.128.97:9008";
+    private String ProcessManagementMicroserviceIp="http://121.5.128.97:9005";
     /*项目状态修改为红*/
     public void ProjectStatusToRed(String projectId) {
         HttpRequest.put(AuditMicroserviceIp + "/v2.0/audit-microservice/projects/status/red?projectId=" + projectId).body();
@@ -32,5 +35,44 @@ public class AdminService {
     /*项目状态修改为黄*/
     public void ProjectStatusToYellow(String projectId) {
         HttpRequest.put(AuditMicroserviceIp + "/v2.0/audit-microservice/projects/status/yellow?projectId=" + projectId).body();
+    }
+
+    /*根据项目id返回对应项目*/
+    public Object findProByProId(String subjectId){
+        String resp= HttpRequest.get(ProjectMicroserviceIp+"/v2.0/project-microservice/projects/Id?id="+
+                subjectId).body();
+        return JSON.parse(resp);
+    }
+
+    /*根据项目id找到所有的反馈*/
+    public Object findFBByProId(String subjectId){
+        String resp= HttpRequest.get(ProcessManagementMicroserviceIp+"/v2.0/processmanagement-microservice/feedback/subjectId?subjectId="+
+                subjectId).body();
+        return JSON.parse(resp);
+    }
+
+    public Object findNotByProId(String subjectId){
+        String resp= HttpRequest.get(FollowMicroserviceIp+"/v2.0/follow-microservice/notice/subjectId?subjectId="+
+                subjectId).body();
+        return JSON.parse(resp);
+    }
+    public Object GetAllComplaint(){
+        String resp= HttpRequest.get(ComplaintMicroserviceIp+"/v2.0/complaint-microservice/complaint/all").body();
+        return JSON.parse(resp);
+    }
+    public void ChangComplaintStatusGreen(String id){
+        HttpRequest.put(ComplaintMicroserviceIp+"/v2.0/complaint-microservice/complaint/status?id="+id
+        +"&status="+"green").body();
+    }
+
+    public void DeleteComplaint(String id){
+        HttpRequest.delete(ComplaintMicroserviceIp+"/v2.0/complaint-microservice/complaintOld?id="+id).body();
+    }
+
+    /*分页返回所有待审核项目*/
+    public Object GetAllYellowProject(String index,String pageSize) {
+        String resp = HttpRequest.get(ProjectMicroserviceIp + "/v2.0/project-microservice/projects/page/yellow?index="+index
+        +"&pageSize="+pageSize).body();
+        return JSON.parse(resp);
     }
 }
