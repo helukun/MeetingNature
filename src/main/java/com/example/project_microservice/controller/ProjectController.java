849d0514f86dd1aa0549a6f2dda92309b4e5a570
LJCARG <83816521+LJCARG@users.noreply.github.com>
2022-12-23 09:28:21 +0800

    Add files via upload

--- src/main/java/com/example/project_microservice/controller/ProjectController.java
+++ src/main/java/com/example/project_microservice/controller/ProjectController.java
@@ -127,7 +127,7 @@ public class ProjectController {
     }
 
     //good
-    @PutMapping("/v2.0/project-microservice/projects/status")
+    @PostMapping("/v2.0/project-microservice/projects/status")
     public void changeProjectStatus(String projectId,String status){
         projectService.changeProjectStatus(projectId,status);
     }
