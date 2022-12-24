74a27c7996edc212d6548837c367fc4115d772c8
Why Not <83454355+skl523@users.noreply.github.com>
2022-12-24 15:40:24 +0800

    Add files via upload

--- src/main/java/com/example/admin_microservice/service/AdminService.java
+++ src/main/java/com/example/admin_microservice/service/AdminService.java
@@ -1,10 +1,13 @@
 package com.example.admin_microservice.service;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.github.kevinsawicki.http.HttpRequest;
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,6 +25,7 @@ public class AdminService {
     private  String ComplaintMicroserviceIp="http://121.5.128.97:9013";
     private String FollowMicroserviceIp="http://121.5.128.97:9008";
     private String ProcessManagementMicroserviceIp="http://121.5.128.97:9005";
+    private String UserMicroserviceIp = "http://121.5.128.97:9007";
     /*项目状态修改为红*/
     public void ProjectStatusToRed(String projectId) {
         HttpRequest.put(AuditMicroserviceIp + "/v2.0/audit-microservice/projects/status/red?projectId=" + projectId).body();
@@ -74,5 +78,40 @@ public class AdminService {
         String resp = HttpRequest.get(ProjectMicroserviceIp + "/v2.0/project-microservice/projects/page/yellow?index="+index
         +"&pageSize="+pageSize).body();
         return JSON.parse(resp);
+    }
+
+    public ResponseEntity<Object> login(String name, String password) {
+        HttpRequest httpRequest=HttpRequest.get(UserMicroserviceIp+"/v2.0/user-microservice/login?name="+name
+                +"&password="+password+"&role="+"3");
+        String res=httpRequest.body();
+        if(httpRequest.badRequest()){
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+        }
+        JSONObject jsonObject =  JSON.parseObject(res);
+        return ResponseEntity.ok(jsonObject);
+    }
+
+    public ResponseEntity<Object> recoverEmail(String name) {
+        Map data=new HashMap<>();
+        data.put("name",name);
+        HttpRequest httpRequest=HttpRequest.post(UserMicroserviceIp+"/v2.0/user-microservice/recoverEmail").form(data);
+        String res=httpRequest.body();
+        if(httpRequest.badRequest()){
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("验证码已发送",HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> recover(String name, String code, String newpassword) {
+        Map data=new HashMap<>();
+        data.put("name",name);
+        data.put("code",code);
+        data.put("newpassword",newpassword);
+        HttpRequest httpRequest=HttpRequest.post(UserMicroserviceIp+"/v2.0/user-microservice/recover").form(data);
+        String res=httpRequest.body();
+        if(httpRequest.badRequest()){
+            return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("新密码修改成功",HttpStatus.OK);
     }
 }
